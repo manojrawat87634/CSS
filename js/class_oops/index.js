95abb90ff4ebbb05f004c11a1a8f4840a3a9c517
Manoj Rawat <manojrawat87634@gmail.com>
2026-02-06 18:13:02 +0530

    inital commit

---
+++ js/class_oops/index.js
@@ -0,0 +1,19 @@
+// class Car{
+//      __name;
+//      constructor (p_nm){
+//        this.__name = p_nm;
+//      }
+//      drive(){
+//         console.log(`This will drive the car ${this.__name}`);
+//      }
+// }
+
+// let c =  new Car("BMW");
+// let c1 =  new Car("Audi");
+// console.log(c.__name);
+// c.drive();
+
+// let date = new Date();
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getFullYear());
